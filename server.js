ee4c997165a5ed61179436dafb61f934d7c94f80
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2021-03-10 10:47:36 -0600

    working towards toggling and adding hike table

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const morgan = require('morgan');
+const PORT = process.env.PORT || 3000;
+const app = express()
+if (!process.env.PORT) app.use(morgan("dev"))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public")); app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
