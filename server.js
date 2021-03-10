b789ca8fd6029672380db951e8b43c6b498dde2d
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2021-03-10 11:10:09 -0600

    heroku deploying to test via https

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require("path")
 const PORT = process.env.PORT || 3000;
 const app = express()
 if (!process.env.PORT) app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public")); app.listen(PORT, () => {
+app.use(express.static("public"))
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
